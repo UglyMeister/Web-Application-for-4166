57613e0b368deae6b312caac4296f91eaef4e6b0
Fred York <fredwashere53@gmail.com>
2019-02-26 18:25:57 -0500

    Assignment 2.5
    placeholder text added to itemDB.js
    images added

--- Assignment/util/itemDB.js
+++ Assignment/util/itemDB.js
@@ -2,10 +2,10 @@ var item = require('../models/item.js');
 //text data for each object
 var dogeText = "This is the doge meme, a common yet widely repsected and revered meme.  This meme has stood the test of time and is more or less a symbol or icon of memery itself.  Disregarding the meme stigma of \"overused memes\", the doge is still a widely used and loved meme format even though it is the most recognizable and most commonly used format to date.Most common uses of this meme format involve ironic or otherwise out of context quotes.  This format is generally used to represet one's confusion or complacence with a given situation or just to state a fact about the current state of reality.";
 var shaggyText = "This is the shaggy meme.  Shaggy is a meme format that exhibits a high amount of irony in that shaggy in the meme is supposedly the strongest being to have ever existed when his appearance is that of a small-bodied individual.  Shaggy memes usually include phrases about Shaggy's unlimited power, for example: \"The dinosaurs going extinct was due to Shaggy exhibiting .1% of his power\".";
-var emojiText = "This is the shaggy meme.  Shaggy is a meme format that exhibits a high amount of irony in that shaggy in the meme is supposedly the strongest being to have ever existed when his appearance is that of a small-bodied individual.  Shaggy memes usually include phrases about Shaggy's unlimited power, for example: \"The dinosaurs going extinct was due to Shaggy exhibiting .1% of his power\".";
-var bText = "This is the shaggy meme.  Shaggy is a meme format that exhibits a high amount of irony in that shaggy in the meme is supposedly the strongest being to have ever existed when his appearance is that of a small-bodied individual.  Shaggy memes usually include phrases about Shaggy's unlimited power, for example: \"The dinosaurs going extinct was due to Shaggy exhibiting .1% of his power\".";
-var blbText = "This is the shaggy meme.  Shaggy is a meme format that exhibits a high amount of irony in that shaggy in the meme is supposedly the strongest being to have ever existed when his appearance is that of a small-bodied individual.  Shaggy memes usually include phrases about Shaggy's unlimited power, for example: \"The dinosaurs going extinct was due to Shaggy exhibiting .1% of his power\".";
-var patrickText = "This is the shaggy meme.  Shaggy is a meme format that exhibits a high amount of irony in that shaggy in the meme is supposedly the strongest being to have ever existed when his appearance is that of a small-bodied individual.  Shaggy memes usually include phrases about Shaggy's unlimited power, for example: \"The dinosaurs going extinct was due to Shaggy exhibiting .1% of his power\".";
+var emojiText = "placeholder";
+var bText = "placeholder";
+var blbText = "placeholder";
+var patrickText = "placeholder";
 
 //follows the model set by item.js
 var shaggy = item.newItem(1,"Shaggy","Modern",shaggyText,"4/5");
@@ -13,7 +13,7 @@ var emoji = item.newItem(2,"Emoji","Modern",emojiText,"4/5");
 var b = item.newItem(3,"B","Modern",bText,"3/5");
 var doge = item.newItem(4,"DOGE","Classic",dogeText,"5/5");
 var blb = item.newItem(5,"Bad Luck Brian","Classic",blbText,"2/5");
-var patrick = item.newItem(6,"Patrick/Spongebob","Classic",patrickText,"4/5");
+var patrick = item.newItem(6,"Patrick and Spongebob","Classic",patrickText,"4/5");
 
 //creates an array consisting of the objects created above
 var getItems = function(){

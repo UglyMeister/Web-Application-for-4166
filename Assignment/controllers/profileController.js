90a445f14d0ba2e84493c4af86a29912d2ab03f9
Fred York <fredwashere53@gmail.com>
2019-03-15 23:08:32 -0400

    2.1
    massive change to profileController.js
    haven't implemented or tested yet, so idk how much of this will need to be reworked in order for the app to not break

--- Assignment/controllers/profileController.js
+++ Assignment/controllers/profileController.js
@@ -1,5 +1,6 @@
 var userProfile = require('../models/userProfile.js');
 var userDB = require('../util/userDB.js');
+var userItem = require('../models/userItem.js');
 var express = require('express');
 var session = require('express-session');
 
@@ -7,7 +8,40 @@ var currentUserProfile;
 
 function main(req, res, next){
   if(req.session.theUser){
+    var currentAction = req.action;
+    if(currentAction == "save" || currentAction == "updateProfile" || currentAction == "updateRating" || currentAction == "updateFlag" || updateRating == "signout"){
+      if(currentAction == "signout"){
+        req.session.destroy();
+      }else{
+        var currentItemList = req.params.itemList;
+        if(currentItemList == null || currentItemList == {} || currentItemList == []){
+          next();
+        }else{
+          var currentItemCode = req.params.itemCode;
+          var exists = false;
+          currentItemList.forEach(element =>{
+            if(element.code == currentItemCode){
+              exists = true;
+            }
+          });
+          if(!exists){
+            next();
+          }else{
+            if(currentAction == "save"){
+              currentItemList.forEach(element =>{
+                if(element.code == currentItemCode){
+                  next();
+                }
+              });
+              var newUserItem = userItem.newUserItem();
+            }
+          }
+        }
 
+      }
+    }else{
+      next();
+    }
   }else{
     req.session.theUser = userDB.getUsers()[0];
     currentUserProfile = new userProfile;

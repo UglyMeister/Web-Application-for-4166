0233de6bf59b8f3bcd757a917c1af7bcaed13f82
Fred York <fredwashere53@gmail.com>
2019-03-18 00:18:52 -0400

    3.5
    minor changes to app.js and user-navigation.ejs
    added save, updateProfile, updateRating, updateFlag, signout functions to profileController2.js and updated if statements and logic

--- Assignment/controllers/profileController2.js
+++ Assignment/controllers/profileController2.js
@@ -2,12 +2,33 @@ var userDB = require('../util/userDB.js');
 
 module.exports = function(){
   return function(req, res, next){
+    //has someone logged in yet?
     if(req.session.theUser){
-      var action = req.params.action;
+      //if there is a specific action that is taking place
+      if(req.params.action){
+        console.log("there is action");
+        if(req.params.action == "save"){//save protocol
 
-      res.redirect('/');
+        }
+        if(req.params.action == "updateProfile"){//update profile protocol
+
+        }
+        if(req.params.action == "updateRating"){//update rating protocol
+
+        }
+        if(req.params.action == "updateFlag"){//update flag protocol
+
+        }
+        if(req.params.action == "signout"){//signout protocol
+          signout();
+        }
+      }else{//there is no specific action taking place
+        res.redirect('/myItems');
+        next();
+      }
 
     }
+    //is the user not logged in right now?
     if(!req.session.theUser || req.session.theUser == "" || req.session.theUser == null){
       var newUser = userDB.getUsers();
       req.session.theUser = newUser;
@@ -17,4 +38,25 @@ module.exports = function(){
     }
 
   }
+}
+
+
+function save(){
+
+}
+
+function updateProfile(){
+
+}
+
+function updateRating(){
+
+}
+
+function updateFlag(){
+
+}
+
+function signout(){
+  req.session.destroy();
 }

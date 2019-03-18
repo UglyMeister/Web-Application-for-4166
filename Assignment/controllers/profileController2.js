f25788897cd70b1f3fdc551074485634ed49dd9b
Fred York <fredwashere53@gmail.com>
2019-03-17 23:26:37 -0400

    3.3
    profileController2.js added check for no user attribute

--- Assignment/controllers/profileController2.js
+++ Assignment/controllers/profileController2.js
@@ -1,7 +1,14 @@
+var userDB = require('../util/userDB.js');
+
 module.exports = function(){
   return function(req, res, next){
-    req.session.theUser = "something";
-    console.log(req);
+    if(!req.session.theUser){
+      var newUser = userDB.getUsers();
+      req.session.theUser = newUser;
+      var newUserProfile = userDB.getUserProfiles();
+      req.session.userProfile = newUserProfile;
+      
+    }
 
 
     next();

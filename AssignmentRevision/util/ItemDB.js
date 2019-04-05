08f61a9fedb02821e5d60cc26509e8cfe8b233c3
Fred York <fredwashere53@gmail.com>
2019-04-05 19:14:27 -0400

    7.5.1 minor change

--- AssignmentRevision/util/ItemDB.js
+++ AssignmentRevision/util/ItemDB.js
@@ -1,68 +1,68 @@
 /* @nanajjar */
 let Item = require('../models/Item');
-let appItems = [];
-
-let itemObj = new Item();
-itemObj.setItemCode(1);
-itemObj.setItemName("Shaggy");
-itemObj.setDescription("Shaggy description");
-itemObj.setCategory("Modern");
-itemObj.setImageURL("images/shaggy.jpg");
-
-appItems.push(itemObj);
-//console.log(appItems.length);
-
-itemObj = new Item();
-itemObj.setItemCode(2);
-itemObj.setItemName("Emoji");
-itemObj.setDescription("Emoji description");
-itemObj.setCategory("Modern");
-itemObj.setImageURL("images/emoji.jpg");
-
-appItems.push(itemObj);
-console.log(appItems.length);
-
-itemObj = new Item();
-itemObj.setItemCode(3);
-itemObj.setItemName("B");
-itemObj.setDescription("B description");
-itemObj.setCategory("Modern");
-itemObj.setImageURL("images/b.jpg");
-
-appItems.push(itemObj);
-
-itemObj = new Item();
-itemObj.setItemCode(4);
-itemObj.setItemName("Doge");
-itemObj.setDescription("Doge description");
-itemObj.setCategory("Classic");
-itemObj.setImageURL("images/doge.jpg");
-
-appItems.push(itemObj);
-
-itemObj = new Item();
-itemObj.setItemCode(5);
-itemObj.setItemName("Bad Luck Brian");
-itemObj.setDescription("Bad Luck Brian description");
-itemObj.setCategory("Classic");
-itemObj.setImageURL("images/BadLuckBrian.jpg");
-
-appItems.push(itemObj);
-
-itemObj = new Item();
-itemObj.setItemCode(6);
-itemObj.setItemName("Patrick and Spongebob");
-itemObj.setDescription("Patrick and Spongebob description");
-itemObj.setCategory("Classic");
-itemObj.setImageURL("images/patrickspongebob.jpg");
-
-appItems.push(itemObj);
-console.log(appItems);
-
-var category1 = [appItems[0], appItems[1], appItems[2]];
-var category2 = [appItems[3], appItems[4], appItems[5]];
-
-var itemsPerCategory = [{ categoryName: "Modern", items: category1 }, { categoryName: "Classic", items: category2 }];
+// let appItems = [];
+//
+// let itemObj = new Item();
+// itemObj.setItemCode(1);
+// itemObj.setItemName("Shaggy");
+// itemObj.setDescription("Shaggy description");
+// itemObj.setCategory("Modern");
+// itemObj.setImageURL("images/shaggy.jpg");
+//
+// appItems.push(itemObj);
+// //console.log(appItems.length);
+//
+// itemObj = new Item();
+// itemObj.setItemCode(2);
+// itemObj.setItemName("Emoji");
+// itemObj.setDescription("Emoji description");
+// itemObj.setCategory("Modern");
+// itemObj.setImageURL("images/emoji.jpg");
+//
+// appItems.push(itemObj);
+// console.log(appItems.length);
+//
+// itemObj = new Item();
+// itemObj.setItemCode(3);
+// itemObj.setItemName("B");
+// itemObj.setDescription("B description");
+// itemObj.setCategory("Modern");
+// itemObj.setImageURL("images/b.jpg");
+//
+// appItems.push(itemObj);
+//
+// itemObj = new Item();
+// itemObj.setItemCode(4);
+// itemObj.setItemName("Doge");
+// itemObj.setDescription("Doge description");
+// itemObj.setCategory("Classic");
+// itemObj.setImageURL("images/doge.jpg");
+//
+// appItems.push(itemObj);
+//
+// itemObj = new Item();
+// itemObj.setItemCode(5);
+// itemObj.setItemName("Bad Luck Brian");
+// itemObj.setDescription("Bad Luck Brian description");
+// itemObj.setCategory("Classic");
+// itemObj.setImageURL("images/BadLuckBrian.jpg");
+//
+// appItems.push(itemObj);
+//
+// itemObj = new Item();
+// itemObj.setItemCode(6);
+// itemObj.setItemName("Patrick and Spongebob");
+// itemObj.setDescription("Patrick and Spongebob description");
+// itemObj.setCategory("Classic");
+// itemObj.setImageURL("images/patrickspongebob.jpg");
+//
+// appItems.push(itemObj);
+// console.log(appItems);
+//
+// var category1 = [appItems[0], appItems[1], appItems[2]];
+// var category2 = [appItems[3], appItems[4], appItems[5]];
+//
+// var itemsPerCategory = [{ categoryName: "Modern", items: category1 }, { categoryName: "Classic", items: category2 }];
 
 
 
